6a0ad9a1ee178f8cffd337a512404f0b91d6f767
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-29 01:26:28 +0900

    SIGN-UP

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// main.jsx
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import { NotFound } from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header.jsx';
+import './Home.css';
+
+function Home() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <h1>Home</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default Home;
